019de96ffc9ed259d46d9cb9ee98eb45e63a6881
Nitesh Kumar <107784137+Niteshkumar7811@users.noreply.github.com>
2023-11-23 11:25:04 +0530

    Update arrays.cpp
    This is code for finding the smallest element ir the array

--- arrays.cpp
+++ arrays.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+using namespace std;
 //Find The second smallest Element in the array
 int secondSmallestElement(int arr[], int n){
     int small = INT_MAX;
@@ -11,4 +12,17 @@ int secondSmallestElement(int arr[], int n){
         else if(secSmall > arr[i] && arr[i] != small) secSmall = arr[i];
     }
     return secSmall;
+}
+
+int main(){
+    int n;
+    cout<<"Take size of array from user: ";
+    cin >> n;
+    int arr[n];
+    cout <<"The input array is: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int ans = secondSmallestElement(arr,n);
+    cout<<"The smallest Element of The array is: "<<ans << endl;
 }
